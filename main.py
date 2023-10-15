2031dcfd88686ec9d9e6bcea0d2f3edbe39ef4ae
Samu <csonka.samuel@gmail.com>
2023-10-15 20:39:02 +0200

    v1 done

--- main.py
+++ main.py
@@ -1,23 +1,60 @@
 import requests
 from bs4 import BeautifulSoup as bs4
+import keyboard
+from time import sleep
+import os
+import random
 
 
-url = str(input("Website:\n"))
+def scrape():
+    os.system('cls')
 
-if "http://" or "https://" not in url:
-    url = "http://" + url
+    url = str(input("Website (e.g.: https://google.com):\n"))
 
-try: 
     response = requests.get(url)
+    
     statCode = response.status_code
 
+    randNum = random.randint(-10000, 100000000)
+
+    text_file = open(f"output{randNum}.txt", "w+")
+
+
     if statCode == 200:
+        
         soup = bs4(response.content, 'html.parser')
         links = soup.find_all('a')
 
         for link in links:
             print(link['href'])
+            text_file.write(link['href'] + '\n')
+        text_file.close()
+        menu()
     else:
         print(f'Failed to retrieve the webpage. Status code: {statCode}')
-except:
-    print(f"Failed to request the given URL: {url}")
+        menu()
+
+def information():
+    os.system('cls')
+    print('soon..')
+    sleep(1)
+    menu()
+
+def menu():
+    os.system('cls')
+    print("[1] Scrape")
+    print("[2] Information")
+    print("[Q] Quit")
+
+    isPressed = False
+    
+    while not isPressed:
+        if keyboard.is_pressed("1"):
+            isPressed = True
+            scrape()
+        if keyboard.is_pressed("2"):
+            isPressed = True
+            information()
+        if keyboard.is_pressed("q"):
+            quit()
+menu()
