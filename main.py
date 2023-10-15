b0983b66af515fadcb2b5c192c3217773f753648
Samu <csonka.samuel@gmail.com>
2023-10-15 19:40:11 +0200

    base

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import requests
 from bs4 import BeautifulSoup as bs4
 
-# Make a request to the website
-url = 'https://google.com'
-response = requests.get(url)
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the HTML content using Beautiful Soup
-    soup = bs4(response.content, 'html.parser')
+url = str(input("Website:\n"))
 
-    # Find all the anchor tags (links)
-    links = soup.find_all('a')
+if "http://" or "https://" not in url:
+    url = "http://" + url
 
-    # Print the links
-    for link in links:
-        print(link['href'])
-else:
-    print('Failed to retrieve the webpage.')
+try: 
+    response = requests.get(url)
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content using Beautiful Soup
+        soup = bs4(response.content, 'html.parser')
+
+        # Find all the anchor tags (links)
+        links = soup.find_all('a')
+
+        # Print the links
+        for link in links:
+            print(link['href'])
+    else:
+        print('Failed to retrieve the webpage.')
+except:
+    print(f"Failed to request the given URL: {url}")
